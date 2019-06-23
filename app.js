e2a70f8f3b9597e05dc4a4697f11ec0afd4aecfd
Noah Giesel <noahgiesel@gmail.com>
2019-06-23 20:13:57 +0200

    finished. need links

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ let trans = () => {
     }, 500)
 }
 
- 
+  
 
 //nav
 let aperto = false;
@@ -61,7 +61,8 @@ const navSlide  = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
-    const body = document.querySelector('body');
+    const body = document.querySelector('body'); 
+    const label = document.querySelector('.sitchTheme');
  
     burger.addEventListener('click',() => {
         nav.classList.toggle('nav-active'); 
@@ -69,10 +70,11 @@ const navSlide  = () => {
         aperto === false ? aperto = true :  aperto = false;
          navLinks.forEach(link =>{
             if(link.style.animation){
- 
+                label.style.animation = '';
                 link.style.animation = '';
             }else {
                 link.style.animation = 'navLinkFade 0.5s ease forwards  .65s'; 
+                label.style.animation = 'navLinkFade 0.5s ease forwards  .65s'; 
            }
          });   
          burger.classList.toggle('toggle');
@@ -84,6 +86,9 @@ function myFunction() {
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li ');
     const body = document.querySelector('body');
+    const label = document.querySelector('.sitchTheme');
+
+
     if ($(window).width() < 960) {
         console.log(aperto); 
 
@@ -95,8 +100,10 @@ function myFunction() {
                 if(link.style.animation){
      
                     link.style.animation = '';
+                    label.style.animation = '';
                 }else {
                     link.style.animation = 'navLinkFade 0.5s ease forwards  .65s'; 
+                    label.style.animation = 'navLinkFade 0.5s ease forwards  .65s'; 
                }
              });
          } 
@@ -107,6 +114,3 @@ function myFunction() {
  
 navSlide();
  
-
-
-
