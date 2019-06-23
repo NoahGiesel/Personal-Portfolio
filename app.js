66e0abc953105b9888e9dfbc49d6acec65609525
Noah Giesel <noahgiesel@gmail.com>
2019-06-23 11:47:45 +0200

    added socials and packages

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
+ 
+
 // PARTICLES
 
  
 
-particlesJS.load('particles-js', './particles.json', function() {
+ particlesJS.load('particles-js', './particles.json', function() { 
+     
 
-    console.log("loaded");
+});
 
-  });
+// color properties of particles depending on HTML theme [dark || light]
 
 
+function changeParticlesColor() {    
+    let currentTheme = document.documentElement.getAttribute('data-theme'); 
+     fetch( "./particles.json") 
+    .then(res => res.json())       
+    .then(function(data) {      
+        let s = JSON.stringify(data.particles.color.value = "red");
+        console.log(s)
+    }); 
+       
 
+}
+
+ 
 
 
 
@@ -21,9 +36,11 @@ checkbox.addEventListener('change', function() {
     if(this.checked) {
         trans()
         document.documentElement.setAttribute('data-theme', 'dark')
+        changeParticlesColor()
     } else {
         trans()
         document.documentElement.setAttribute('data-theme', 'light')
+        changeParticlesColor()
     }
 })
 
@@ -89,3 +106,7 @@ function myFunction() {
  
  
 navSlide();
+ 
+
+
+
