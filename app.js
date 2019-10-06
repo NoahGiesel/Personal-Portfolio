6b27040c4193d9c5efeb4e32968a325de29cf966
noahgiesel <noahgiesel@gmail.com>
2019-10-06 16:04:15 +0200

    fixed explorer allert

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-/* Sample function that returns boolean in case the browser is Internet Explorer*/
-function isIE() {
-    ua = navigator.userAgent;
-    /* MSIE used to detect old browsers and Trident used to newer ones*/
-    var is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
-    
-    return is_ie; 
-  }
-  /* Create an alert to show if the browser is IE or not */
-  if (isIE()){
-      alert('It is InternetExplorer');
-  } 
+
+    /* Sample function that returns boolean in case the browser is Internet Explorer*/
+    function isIE() {
+        ua = navigator.userAgent;
+        /* MSIE used to detect old browsers and Trident used to newer ones*/
+        var is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
+        
+        return is_ie; 
+    }
+
+    /* Create an alert to show if the browser is IE or not */
+    if (isIE()){
+        alert('It is InternetExplorer');
+    } 
 
 // PARTICLES
 
